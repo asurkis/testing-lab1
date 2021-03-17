5c6e0652c3c3992e32c99246ff02fd264892fe7f
Anton Surkis <antonsurkis@gmail.com>
2021-03-18 00:19:47 +0300

    Added domain model

---
+++ src/main/kotlin/DomainModel.kt
@@ -0,0 +1,65 @@
+interface ObjectFromEarth
+
+typealias Location = MutableCollection<PhysicalObject>
+
+open class PhysicalObject(location: Location?) {
+    var location: Location? = null
+
+    init {
+        moveTo(location)
+    }
+
+    fun moveTo(anotherLocation: Location?) {
+        location?.remove(this)
+        anotherLocation?.add(this)
+        location = anotherLocation
+    }
+}
+
+open class PhysicalContainer(location: Location?) : PhysicalObject(location), Location {
+    val elements: Location = mutableListOf()
+
+    override val size: Int
+        get() = elements.size
+
+    override fun contains(element: PhysicalObject) = elements.contains(element)
+    override fun containsAll(elements: Collection<PhysicalObject>) = this.elements.containsAll(elements)
+    override fun isEmpty() = elements.isEmpty()
+    override fun add(element: PhysicalObject) = elements.add(element)
+    override fun addAll(elements: Collection<PhysicalObject>) = this.elements.addAll(elements)
+    override fun clear() = elements.clear()
+    override fun iterator() = elements.iterator()
+    override fun remove(element: PhysicalObject) = elements.remove(element)
+    override fun removeAll(elements: Collection<PhysicalObject>) = this.elements.removeAll(elements)
+    override fun retainAll(elements: Collection<PhysicalObject>) = this.elements.retainAll(elements)
+}
+
+open class Human(val name: String, location: Location?) : PhysicalObject(location) {
+    open val isAnxious get() = false
+    val hands = PhysicalContainer(null)
+}
+
+class HumanFromBetelgeuse(name: String, location: Location?) : Human(name, location)
+
+class HumanFromEarth(name: String, location: Location?) : Human(name, location),
+    ObjectFromEarth {
+    override val isAnxious get() = location?.all { it == this || it !is ObjectFromEarth } != false
+}
+
+class DentrassiLingerie(location: Location?) : PhysicalObject(location)
+class SkvornshellMattress(location: Location?) : PhysicalObject(location)
+class Fish(location: Location?, color: String) : PhysicalObject(location)
+
+class FishContainer(location: Location?) : PhysicalContainer(location) {
+    override fun add(element: PhysicalObject): Boolean {
+        if (element !is Fish) throw TypeCastException("FishContainer can only contain Fish")
+        return super.add(element)
+    }
+
+    override fun addAll(elements: Collection<PhysicalObject>): Boolean {
+        if (!elements.all { it is Fish }) throw TypeCastException("FishContainer can only contain Fish")
+        return super.addAll(elements)
+    }
+}
+
+class BagOfCornflakes(location: Location?) : PhysicalObject(location), ObjectFromEarth
--- src/test/kotlin/DomainModelTest.kt
+++ src/test/kotlin/DomainModelTest.kt
@@ -1,2 +1,38 @@
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import kotlin.test.assertNotEquals
+
 class DomainModelTest {
+    @ParameterizedTest
+    @ValueSource(booleans = [ false, true ])
+    fun testText(addCornflakes: Boolean) {
+        val room = mutableListOf<PhysicalObject>()
+        val arthur = HumanFromEarth("Arthur", room)
+        val ford = HumanFromBetelgeuse("Ford", room)
+        val fishContainer = FishContainer(ford.hands)
+        val dentrassiLingerie = DentrassiLingerie(room)
+        val skvornshellMattress = SkvornshellMattress(room)
+        val yellowFish = Fish(fishContainer, "yellow")
+
+        if (addCornflakes) {
+            val cornflakes = BagOfCornflakes(room)
+        }
+
+        assertNotEquals(addCornflakes, arthur.isAnxious)
+    }
+
+    @Test
+    fun testFishContainer() {
+        val fishContainer = FishContainer(null)
+        val fish = Fish(fishContainer, "qwerqwer")
+        assert(fishContainer.contains(fish))
+    }
+
+    @Test
+    fun testFishContainerIncorrect() {
+        val fishContainer = FishContainer(null)
+        assertThrows<TypeCastException> { DentrassiLingerie(fishContainer) }
+    }
 }
