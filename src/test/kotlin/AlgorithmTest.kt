b68aeb30c1f9124f06ebf1adf0b9cf8ed1312039
Anton <antonsurkis@gmail.com>
2021-04-01 16:25:24 +0300

    Added testing invariant

--- src/test/kotlin/AlgorithmTest.kt
+++ src/test/kotlin/AlgorithmTest.kt
@@ -41,6 +41,23 @@ class AlgorithmTest {
 
     @ParameterizedTest
     @ValueSource(ints = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 31, 32, 33, 63, 64, 65, 100])
+    fun testInvariant(n: Int) {
+        val btree = BTree()
+        for (i in 1..n) btree.insert(i)
+        Assertions.assertTrue(btree.invariantCorrect)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 31, 32, 33, 63, 64, 65, 100])
+    fun testInvariantRemoving(n: Int) {
+        val btree = BTree()
+        for (i in 1..n) btree.insert(i)
+        for (i in 1..n / 2) btree.remove(i)
+        Assertions.assertTrue(btree.invariantCorrect)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 31, 32, 33, 63, 64, 65, 100])
     fun testRemoving(n: Int) {
         val btree = BTree()
         for (i in 1..n) btree.insert(i)
